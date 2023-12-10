4bb40f5bc0de005f1ff5d85426d7c532ca140464
BilaalAnwar <32581768+BilaalAnwar@users.noreply.github.com>
2023-12-10 20:04:10 +0500

    initial

---
+++ MasterMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasterMovie
+//
+//  Created by Bilal Anwar on 12/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
